87bb2b2917f6ed6b9fdecaf59af108da2d53eb41
Fledtrain <Joeyew2014@gmail.com>
2023-04-02 23:39:19 -0400

    Adding files

---
+++ src/data.js
@@ -0,0 +1,112 @@
+export const questions = [
+  {
+    id: 1,
+    question: "What animal has the longest lifespan?",
+    answerId: 2,
+    options: [
+      { id: 1, text: "Blue Whale" },
+      { id: 2, text: "Giant Tortoise" },
+      { id: 3, text: "Galapagos Tortoise" },
+      { id: 4, text: "African Elephant" },
+    ],
+  },
+  {
+    id: 2,
+    question: "What is the largest country in the world?",
+    answerId: 1,
+    options: [
+      { id: 1, text: "Russia" },
+      { id: 2, text: "Canada" },
+      { id: 3, text: "China" },
+      { id: 4, text: "United States" },
+    ],
+  },
+  {
+    id: 3,
+    question: "What is the fastest flying bird in the world?",
+    answerId: 1,
+    options: [
+      { id: 1, text: "Peregrine Falcon" },
+      { id: 2, text: "Golden Eagle" },
+      { id: 3, text: "Sparrow Hawk" },
+      { id: 4, text: "Kestrel" },
+    ],
+  },
+  {
+    id: 4,
+    question: "A newborn kangaroo is about the size of a ....",
+    answerId: 3,
+    options: [
+      { id: 1, text: "Grape" },
+      { id: 2, text: "Lemon" },
+      { id: 3, text: "Lima Bean" },
+      { id: 4, text: "Cherry" },
+    ],
+  },
+  {
+    id: 5,
+    question: "What is the smallest planet in our solar system?",
+    answerId: 1,
+    options: [
+      { id: 1, text: "Mercury" },
+      { id: 2, text: "Venus" },
+      { id: 3, text: "Mars" },
+      { id: 4, text: "Earth" },
+    ],
+  },
+  {
+    id: 6,
+    question: "Which country is both an island and a continent?",
+    answerId: 2,
+    options: [
+      { id: 1, text: "North America" },
+      { id: 2, text: "Australia" },
+      { id: 3, text: "South America" },
+      { id: 4, text: "Antarctica" },
+    ],
+  },
+  {
+    id: 7,
+    question: "Which planet in our solar system has the most moons?",
+    answerId: 4,
+    options: [
+      { id: 1, text: "Venus" },
+      { id: 2, text: "Mars" },
+      { id: 3, text: "Earth" },
+      { id: 4, text: "Jupiter" },
+    ],
+  },
+  {
+    id: 8,
+    question: "What is the largest mammal in the world?",
+    answerId: 3,
+    options: [
+      { id: 1, text: "Elephant" },
+      { id: 2, text: "Hippopotamus" },
+      { id: 3, text: "Blue Whale" },
+      { id: 4, text: "Giraffe" },
+    ],
+  },
+  {
+    id: 9,
+    question: "What is the capital city of Japan?",
+    answerId: 2,
+    options: [
+      { id: 1, text: "Shanghai" },
+      { id: 2, text: "Tokyo" },
+      { id: 3, text: "Beijing" },
+      { id: 4, text: "Seoul" },
+    ],
+  },
+  {
+    id: 10,
+    question: "What is the largest desert in the world?",
+    answerId: 3,
+    options: [
+      { id: 1, text: "Sahara Desert" },
+      { id: 2, text: "Mojave Desert" },
+      { id: 3, text: "Antarctic Desert" },
+      { id: 4, text: "Gobi Desert" },
+    ],
+  },
+];
